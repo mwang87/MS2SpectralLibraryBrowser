06efd8e1bb7b28d327b661b4cf5fcb2a3020df65
mingxun <mingxun@ubuntu.(none)>
2015-05-08 21:17:09 -0700

    prepping the gems we need

--- settings.rb
+++ settings.rb
@@ -4,8 +4,8 @@ PAGINATION_SIZE = 30
 
 puts ENV["RACK_ENV"]
 configure :test do
-    DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/data.db")
-	#DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/proxi-demo' )
+    #DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/data.db")
+	DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/spectrallibrarybrowser' )
 end
 configure :development do
     DataMapper.setup(:default, ENV['DATABASE_URL'] )
