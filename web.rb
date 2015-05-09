b1744caa7e994fa5c4d345309e38732f30bcd3cf
mingxun <mingxun@ubuntu.(none)>
2015-05-08 22:20:18 -0700

    enriching the spectrum view to query on anything

--- web.rb
+++ web.rb
@@ -7,6 +7,7 @@ require 'data_mapper'
 require 'dm-transactions'
 require './settings'
 require './models'
+require './utils'
 
 class Sinatra::Application < Sinatra::Base
 end
@@ -18,157 +19,52 @@ get '/' do
 end
 
 
-get '/spectra/library/:libraryid' do
-    page_number = 1
-    if params[:page] != nil
-        page_number = params[:page].to_i
-    end
-    
-    library_db = Library.first(params[:libraryname])
-    
-    
-    @library_spectra = library_db.spectrum.all(:offset => (page_number - 1) * PAGINATION_SIZE , :limit => PAGINATION_SIZE)
-    
-    #Determining next and prev page
-    if page_number == 1
-        @next_page = page_number + 1
-        @previous_page = nil
-    else
-        @next_page = page_number + 1
-        @previous_page = page_number - 1
-    end
-    
-    @request_path = request.path
+get '/spectra/aggregateview' do
+    page_number, @previous_page, @next_page = page_prev_next_utilties(params)
+    @page_number = page_number
+
+    @all_libraries = Library.all().map(&:name)
+
+    @query_variant = params[:variant]
+    @query_peptide = params[:peptide]
+    @query_library = params[:library]
+
+    query_parameters = Hash.new
+    query_parameters[:offset]  = (page_number - 1) * PAGINATION_SIZE
+    query_parameters[:limit]  = PAGINATION_SIZE
+    count_parameters = Hash.new
+
     @param_string = ""
-    
-    @display_string = "Library View: " + library_db.name
-    
+
+    if @query_variant != nil and @query_variant.length > 2
+        query_parameters[:peptide.like] = "%" + @query_variant + "%"
+        @param_string += "&variant=" + @query_variant
+    end
+
+    if @query_peptide != nil and @query_peptide.length > 2
+       query_parameters[:unmodifiedpeptide.like] = "%" + @query_peptide + "%"
+       @param_string += "&peptide=" + @query_peptide
+    end
+ 
+    if @query_library != nil and @query_library.length > 2
+        query_parameters[:library] = Library.first(:name => @query_library)
+        @param_string += "&library=" + @query_library
+    end
+
+    puts query_parameters
+
+    @all_spectra = Spectrum.all(query_parameters)
+    @total_count = Spectrum.count(count_parameters)
+
+    if (@next_page - 1) * PAGINATION_SIZE > @total_count
+        @next_page = nil
+    end
+
     haml :spectra
 end
-
 
 get '/spectrum/:id' do
     @library_spectrum = Spectrum.get(params[:id])
     
     haml :spectrum
-end
-
-get '/spectra/querysequence' do
-    query_peptide = params[:sequence]
-    
-    page_number = 1
-    if params[:page] != nil
-        page_number = params[:page].to_i
-    end
-    
-    @library_spectra = Spectrum.all(:offset => (page_number - 1) * PAGINATION_SIZE, :limit => PAGINATION_SIZE, :unmodifiedpeptide => query_peptide)
-    
-    #results_count = Spectrum.count(:unmodifiedpeptide.like => query_peptide)
-    
-    #Determining next and prev page
-    if page_number == 1
-        @next_page = page_number + 1
-        @previous_page = nil
-    else
-        @next_page = page_number + 1
-        @previous_page = page_number - 1
-    end
-    
-    @param_string = "sequence="  + params[:sequence]
-    @request_path = request.path
-    
-    @display_string = "Sequence Query: " + params[:sequence]
-    
-    haml :spectra
-end
-
-get '/spectra/querysequencesubstring' do
-    query_peptide = params[:sequence]
-    
-    page_number = 1
-    if params[:page] != nil
-        page_number = params[:page].to_i
-    end
-    
-    query_peptide =  "%" + query_peptide + "%"
-    
-    @library_spectra = Spectrum.all(:offset => (page_number - 1) * PAGINATION_SIZE, :limit => PAGINATION_SIZE, :unmodifiedpeptide.like => query_peptide)
-    
-    #results_count = Spectrum.count(:unmodifiedpeptide.like => query_peptide)
-    
-    #Determining next and prev page
-    if page_number == 1
-        @next_page = page_number + 1
-        @previous_page = nil
-    else
-        @next_page = page_number + 1
-        @previous_page = page_number - 1
-    end
-    
-    @param_string = "sequence="  + params[:sequence]
-    @request_path = request.path
-    
-    @display_string = "Sequence Query: " + params[:sequence]
-    
-    haml :spectra
-end
-
-get '/spectra/querypeptide' do
-    query_peptide = params[:sequence]
-    
-    page_number = 1
-    if params[:page] != nil
-        page_number = params[:page].to_i
-    end
-    
-    @library_spectra = Spectrum.all(:offset => (page_number - 1) * PAGINATION_SIZE, :limit => PAGINATION_SIZE, :peptide => query_peptide)
-    
-    #results_count = Spectrum.count(:peptide.like => query_peptide)
-    
-    #Determining next and prev page
-    if page_number == 1
-        @next_page = page_number + 1
-        @previous_page = nil
-    else
-        @next_page = page_number + 1
-        @previous_page = page_number - 1
-    end
-    
-    @param_string = "sequence="  + params[:sequence]
-    @request_path = request.path
-    
-    @display_string = "Peptide Query: " + params[:sequence]
-    
-    haml :spectra
-end
-
-get '/spectra/querypeptidesubstring' do
-    query_peptide = params[:sequence]
-    
-    page_number = 1
-    if params[:page] != nil
-        page_number = params[:page].to_i
-    end
-    
-    query_peptide =  "%" + query_peptide + "%"
-    
-    @library_spectra = Spectrum.all(:offset => (page_number - 1) * PAGINATION_SIZE, :limit => PAGINATION_SIZE, :peptide.like => query_peptide)
-    
-    #results_count = Spectrum.count(:peptide.like => query_peptide)
-    
-    #Determining next and prev page
-    if page_number == 1
-        @next_page = page_number + 1
-        @previous_page = nil
-    else
-        @next_page = page_number + 1
-        @previous_page = page_number - 1
-    end
-    
-    @param_string = "sequence="  + params[:sequence]
-    @request_path = request.path
-    
-    @display_string = "Peptide Query: " + params[:sequence]
-    
-    haml :spectra
 end
