a68e7a8211d27707e103c64f27c0d3ac8d5ab52d
mingxun <mingxun@ubuntu.(none)>
2015-05-08 21:23:50 -0700

    removing new relic

--- settings.rb
+++ settings.rb
@@ -4,14 +4,13 @@ PAGINATION_SIZE = 30
 
 puts ENV["RACK_ENV"]
 configure :test do
-    #DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/data.db")
-	DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/spectrallibrarybrowser' )
+    DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/data.db")
+	#DataMapper.setup(:default, 'postgres://postgres:postgres@localhost/spectrallibrarybrowser' )
 end
 configure :development do
     DataMapper.setup(:default, ENV['DATABASE_URL'] )
 end
 configure :production do
-	require 'newrelic_rpm'
     DataMapper.setup(:default, ENV['DATABASE_URL'] )
 end
 
