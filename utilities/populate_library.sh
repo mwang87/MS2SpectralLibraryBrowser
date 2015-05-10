f66b6e7d3af6c8db82f7de3a9c28bfac98491f9e
mingxun <mingxun@ubuntu.(none)>
2015-05-09 23:07:49 -0700

    adding batch add libraries

--- utilities/populate_library.sh
+++ utilities/populate_library.sh
@@ -1,4 +1,20 @@
-ruby ./populate_library.rb ./yeast_target.mgf YEAST
+#ruby ./populate_library.rb ./yeast_target.mgf YEAST
 #ruby ./populate_library.rb ./utilities/c_elegans_spectrast.mgf C_ELEGANS
 #ruby ./populate_library.rb ./utilities/human_6_05.mgf HUMAN
+
+
+ruby ./populate_library.rb ./libraries/bsa_2009_05_06_it.mgf NIST_BSA_IT_2009
+ruby ./populate_library.rb ./libraries/c_elegans_2011_05_24_it.mgf NIST_C_Elegans_IT_2011
+ruby ./populate_library.rb ./libraries/chicken_2011_05_24_it.mgf NIST_Chicken_IT_2011
+ruby ./populate_library.rb ./libraries/e_coli_2013_05_09_it.mgf NIST_E_Coli_IT_2013
+ruby ./populate_library.rb ./libraries/human_2013_06_03_hcd.mgf NIST_Human_HCD_2013
+ruby ./populate_library.rb ./libraries/human_2014_05_29_it.mgf NIST_Human_IT_2014
+ruby ./populate_library.rb ./libraries/mouse_2013_05_20_it.mgf NIST_Mouse_IT_2013
+ruby ./populate_library.rb ./libraries/rat_2013_05_30_it.mgf NIST_RAT_IT_2013
+ruby ./populate_library.rb ./libraries/rat_2013_06_05_qtof.mgf NIST_RAT_QTOF_2013
+ruby ./populate_library.rb ./libraries/sigmaups1_2011_05_24_it.mgf NIST_SIGMAUPS_IT_2011
+ruby ./populate_library.rb ./libraries/yeast_2012_04_06_it.mgf NIST_Yeast_IT_2012
+ruby ./populate_library.rb ./libraries/yeast_2012_04_17_qtof.mgf NIST_Yeast_QTOF_2012
+ruby ./populate_library.rb ./libraries/yeast_pombe_2013_05_18_it.mgf NIST_Yeast_Pombe_IT_2013
+
 
